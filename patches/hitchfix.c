4bba09f4b3162ce782d79709de4c531580ab1098
Garrett Smith <smith.garrett.c@gmail.com>
2026-01-03 00:05:07 -0800

    fix the hitchfix

--- patches/hitchfix.c
+++ patches/hitchfix.c
@@ -4,12 +4,15 @@ extern ALSeqPlayer* D_80248C90;
 void func_80206150(s32 arg0);
 f64 uvClkGetSec(s32 clk_id);
 
+void yield_self_1ms(void);
+
 RECOMP_PATCH void uvaSeqStop(void) {
     alSeqpStop(D_80248C90);
     func_80206150(7);
     if (alSeqpGetState(D_80248C90) != 0) {
         do {
-            if (uvClkGetSec(7) > 0.001) { // reduced from 2 seconds -- thread isn't yielding??
+            yield_self_1ms();
+            if (uvClkGetSec(7) > 2.0) {
                 _uvDebugPrintf("uvaSeqStop timed out\n");
                 return;
             }
